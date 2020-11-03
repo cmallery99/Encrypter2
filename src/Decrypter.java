185ce82c7141227230e2d175d8f8df03bd1c7c98
condor72199 <cmallery99@gmail.com>
2020-11-02 22:27:07 -0500

    Optimizations to Encrypter and Decrypter.

--- src/Decrypter.java
+++ src/Decrypter.java
@@ -7,68 +7,67 @@ public class Decrypter {
         int[] in = a;
         int seed = b;
         Random random = new Random(seed);
-        HashMap<String,String> map = new HashMap<String,String>();
+        HashMap<Integer,String> map = new HashMap<Integer, String>();
 
-        map.put("1","a");
-        map.put("2","b");
-        map.put("3","c");
-        map.put("4","d");
-        map.put("5","e");
-        map.put("6","f");
-        map.put("7","g");
-        map.put("8","h");
-        map.put("9","i");
-        map.put("10","j");
-        map.put("11","k");
-        map.put("12","l");
-        map.put("13","m");
-        map.put("14","n");
-        map.put("15","o");
-        map.put("16","p");
-        map.put("17","q");
-        map.put("18","r");
-        map.put("19","s");
-        map.put("20","t");
-        map.put("21","u");
-        map.put("22","v");
-        map.put("23","w");
-        map.put("24","x");
-        map.put("25","y");
-        map.put("26","z");
-        map.put("27","A");
-        map.put("28","B");
-        map.put("29","C");
-        map.put("30","D");
-        map.put("31","E");
-        map.put("32","F");
-        map.put("33","G");
-        map.put("34","H");
-        map.put("35","I");
-        map.put("36","J");
-        map.put("37","K");
-        map.put("38","L");
-        map.put("39","M");
-        map.put("40","N");
-        map.put("41","O");
-        map.put("42","P");
-        map.put("43","Q");
-        map.put("44","R");
-        map.put("45","S");
-        map.put("46","T");
-        map.put("47","U");
-        map.put("48","V");
-        map.put("49","W");
-        map.put("50","X");
-        map.put("51","Y");
-        map.put("52","Z");
-        map.put("53","_");
-        map.put("54"," ");
+        map.put(1,"a");
+        map.put(2,"b");
+        map.put(3,"c");
+        map.put(4,"d");
+        map.put(5,"e");
+        map.put(6,"f");
+        map.put(7,"g");
+        map.put(8,"h");
+        map.put(9,"i");
+        map.put(10,"j");
+        map.put(11,"k");
+        map.put(12,"l");
+        map.put(13,"m");
+        map.put(14,"n");
+        map.put(15,"o");
+        map.put(16,"p");
+        map.put(17,"q");
+        map.put(18,"r");
+        map.put(19,"s");
+        map.put(20,"t");
+        map.put(21,"u");
+        map.put(22,"v");
+        map.put(23,"w");
+        map.put(24,"x");
+        map.put(25,"y");
+        map.put(26,"z");
+        map.put(27,"A");
+        map.put(28,"B");
+        map.put(29,"C");
+        map.put(30,"D");
+        map.put(31,"E");
+        map.put(32,"F");
+        map.put(33,"G");
+        map.put(34,"H");
+        map.put(35,"I");
+        map.put(36,"J");
+        map.put(37,"K");
+        map.put(38,"L");
+        map.put(39,"M");
+        map.put(40,"N");
+        map.put(41,"O");
+        map.put(42,"P");
+        map.put(43,"Q");
+        map.put(44,"R");
+        map.put(45,"S");
+        map.put(46,"T");
+        map.put(47,"U");
+        map.put(48,"V");
+        map.put(49,"W");
+        map.put(50,"X");
+        map.put(51,"Y");
+        map.put(52,"Z");
+        map.put(53,"_");
+        map.put(54," ");
 
         int length = Array.getLength(a);
 
         int[] seedInts = new int[length];
         int[] newInts = new int[length];
-        String[] intsString = new String[length];
         String[] charsString = new String[length];
 
 
@@ -78,11 +77,9 @@ public class Decrypter {
         for (int i = 0; i < length; i++) {
             newInts[i] = (in[i] / seedInts[i]);
         }
+
         for (int i = 0; i < length; i++) {
-            intsString[i] = Integer.toString(newInts[i]);
-        }
-        for (int i = 0; i < length; i++) {
-            charsString[i] = map.get(intsString[i]);
+            charsString[i] = map.get(newInts[i]);
         }
         String ans = "";
         for (int i = 0; i < length; i++) {
--- src/Encrypter.java
+++ src/Encrypter.java
@@ -3,62 +3,62 @@ import java.util.Random;
 
 public class Encrypter {
     public static int[] encrypt(String a,int b) {
-        HashMap<String,String> map = new HashMap<String,String>();
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
 
-        map.put("a","1");
-        map.put("b","2");
-        map.put("c","3");
-        map.put("d","4");
-        map.put("e","5");
-        map.put("f","6");
-        map.put("g","7");
-        map.put("h","8");
-        map.put("i","9");
-        map.put("j","10");
-        map.put("k","11");
-        map.put("l","12");
-        map.put("m","13");
-        map.put("n","14");
-        map.put("o","15");
-        map.put("p","16");
-        map.put("q","17");
-        map.put("r","18");
-        map.put("s","19");
-        map.put("t","20");
-        map.put("u","21");
-        map.put("v","22");
-        map.put("w","23");
-        map.put("x","24");
-        map.put("y","25");
-        map.put("z","26");
-        map.put("A","27");
-        map.put("B","28");
-        map.put("C","29");
-        map.put("D","30");
-        map.put("E","31");
-        map.put("F","32");
-        map.put("G","33");
-        map.put("H","34");
-        map.put("I","35");
-        map.put("J","36");
-        map.put("K","37");
-        map.put("L","38");
-        map.put("M","39");
-        map.put("N","40");
-        map.put("O","41");
-        map.put("P","42");
-        map.put("Q","43");
-        map.put("R","44");
-        map.put("S","45");
-        map.put("T","46");
-        map.put("U","47");
-        map.put("V","48");
-        map.put("W","49");
-        map.put("X","50");
-        map.put("Y","51");
-        map.put("Z","52");
-        map.put("_","53");
-        map.put(" ","54");
+        map.put("a",1);
+        map.put("b",2);
+        map.put("c",3);
+        map.put("d",4);
+        map.put("e",5);
+        map.put("f",6);
+        map.put("g",7);
+        map.put("h",8);
+        map.put("i",9);
+        map.put("j",10);
+        map.put("k",11);
+        map.put("l",12);
+        map.put("m",13);
+        map.put("n",14);
+        map.put("o",15);
+        map.put("p",16);
+        map.put("q",17);
+        map.put("r",18);
+        map.put("s",19);
+        map.put("t",20);
+        map.put("u",21);
+        map.put("v",22);
+        map.put("w",23);
+        map.put("x",24);
+        map.put("y",25);
+        map.put("z",26);
+        map.put("A",27);
+        map.put("B",28);
+        map.put("C",29);
+        map.put("D",30);
+        map.put("E",31);
+        map.put("F",32);
+        map.put("G",33);
+        map.put("H",34);
+        map.put("I",35);
+        map.put("J",36);
+        map.put("K",37);
+        map.put("L",38);
+        map.put("M",39);
+        map.put("N",40);
+        map.put("O",41);
+        map.put("P",42);
+        map.put("Q",43);
+        map.put("R",44);
+        map.put("S",45);
+        map.put("T",46);
+        map.put("U",47);
+        map.put("V",48);
+        map.put("W",49);
+        map.put("X",50);
+        map.put("Y",51);
+        map.put("Z",52);
+        map.put("_",53);
+        map.put(" ",54);
 
 
         int seed = b;
@@ -68,7 +68,6 @@ public class Encrypter {
 
         int length = s.length();
         String[] cr = new String[length];
-        String[] in = new String[length];
         int[] ina = new int[length];
         int[] seedInts = new int[length];
         int[] ans = new int[length];
@@ -79,11 +78,7 @@ public class Encrypter {
         }
         //maps letters to numbers(strings)
         for (int i = 0; i < length; i++) {
-            in[i] = map.get(cr[i]);
-        }
-        // converts array of strings to array of ints
-        for (int i = 0; i < length; i++) {
-            ina[i] = Integer.parseInt(in[i]);
+            ina[i] = map.get(cr[i]);
         }
         // generate random int array
         for (int i = 0; i < length; i++) {
