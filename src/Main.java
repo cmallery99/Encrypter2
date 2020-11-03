6164f4c04e9b175a42c1fa601db711a833e76081
condor72199 <cmallery99@gmail.com>
2020-11-02 21:57:43 -0500

    V2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -7,37 +12,87 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
 
-        System.out.print("Encrypt or Decrypt: ");
-        String ed = input.nextLine();
+        System.out.println(Arrays.toString(args));
 
-        if (ed.equals("Encrypt")) {
+        String ed = args[0];
 
-            System.out.print("Enter seed or enter zero for random: ");
-            int seed = input.nextInt();
 
-            if (seed == 0) {
-                seed = random.nextInt(10000);
+        if (ed.equals("-e")) {
+
+            String sEncrypt = args[1];
+
+            String filePath = args[2];
+
+            try {
+                File file = new File(filePath);
+                if (file.createNewFile()) {
+                    System.out.println("File created: " + file.getName());
+                }
+                else {
+                    System.out.println("File already exists.");
+                }
+            }
+            catch (IOException e) {
+                System.out.println("An error has occurred.");
+                e.printStackTrace();
             }
 
-            System.out.print("Enter string to encrypt: ");
-            String s = input.next();
-            int[] i = Encrypter.encrypt(s, seed);
+            try {
+                FileWriter writer = new FileWriter(filePath);
 
-            System.out.println(Arrays.toString(i) + seed);
+                int seed = random.nextInt(10000 );
+
+                int[] ia = Encrypter.encrypt(sEncrypt, seed);
+
+                int length = ia.length;
+
+                for (int i = 0; i < length; i++ ) {
+                    writer.write(String.valueOf(ia[i]) + System.getProperty("line.separator"));
+                }
+
+                writer.append(String.valueOf(seed));
+                writer.close();
+                System.out.println("Successfully wrote to file.");
+
+            }
+            catch (IOException e) {
+                System.out.println("An error has occurred.");
+                e.printStackTrace();
+            }
+
         }
-        if (ed.equals("Decrypt")) {
+        if (ed.equals("-d")) {
 
-            System.out.print("Enter seed: ");
-            int seed = input.nextInt();
+            String filePath = args[1];
 
-            System.out.print("Enter length of message: ");
-            int length = input.nextInt();
+            ArrayList<String> in = new ArrayList<>();
+
+            try {
+                File file = new File(filePath);
+                Scanner reader = new Scanner(file);
+
+                while (reader.hasNextLine()) {
+                    in.add(reader.nextLine());
+                }
+
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+
+            int length = in.size();
+
+            int seed = Integer.parseInt(in.get(length - 1));
+
+            in.remove(length-1);
+
+            length = in.size();
 
             int[] message = new int[length];
 
             for (int i = 0; i < length; i++) {
-                System.out.print("Enter number: ");
-                message[i] = input.nextInt();
+                message[i] = Integer.parseInt(in.get(i));
             }
 
             String ans = Decrypter.decrypt(message,seed);
